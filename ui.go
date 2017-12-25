9748b5a25ad6cf19d02cc82eae8fe5b34d79cef8
Mechiel Lukkien <mechiel@ueber.net>
2017-12-25 16:17:43 +0100

    slightly better UI function signatures

--- ui.go
+++ ui.go
@@ -56,24 +56,24 @@ type UI interface {
 	// Layout asks the UI to lay itself out with a max size of `r`.
 	// The UI can lay itself out beyong size.Y, not beyond size.X.
 	// size.Y is the amount of screen real estate that will still be visible.
-	Layout(env *Env, size image.Point) image.Point
+	Layout(env *Env, sizeAvail image.Point) (sizeTaken image.Point)
 
-	// Draw asks the UI to draw itself on `img`, with an offset of `orig`.
+	// Draw asks the UI to draw itself on `img`, with `orig` as offset.
 	Draw(env *Env, img *draw.Image, orig image.Point, m draw.Mouse)
 
 	// Mouse tells the UI about mouse movement over it.
 	// It can also be called when the mouse moved out of the UI. This facilitates redrawing after leaving the UI element, to draw it in non-hovered form. The UI is responsible for determining if the mouse is over the UI or not.
 	// Result is used to tell the caller whether the event was consumed, and whether UI's need to be redrawn, etc.
-	Mouse(env *Env, m draw.Mouse) (result Result)
+	Mouse(env *Env, m draw.Mouse) (r Result)
 
 	// Key tells the UI about a key press over it.
-	Key(env *Env, orig image.Point, m draw.Mouse, k rune) (result Result)
+	Key(env *Env, orig image.Point, m draw.Mouse, k rune) (r Result)
 
 	// FirstFocus returns the top-left corner where the focus should go next when "tab" is hit, if anything.
-	FirstFocus(env *Env) *image.Point
+	FirstFocus(env *Env) (warp *image.Point)
 
 	// Focus returns the focus-point for `ui`.
-	Focus(env *Env, o UI) *image.Point
+	Focus(env *Env, o UI) (warp *image.Point)
 
 	// Print line about ui that includes r and is prefixed with indent spaces, following by a Print on each child.
 	Print(indent int, r image.Rectangle)
