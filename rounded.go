a68345d066a30140fc2c96a2d2b9f0fbc2d6118e
Mechiel Lukkien <mechiel@ueber.net>
2017-12-25 09:36:31 +0100

    better rounded corners

--- rounded.go
+++ rounded.go
@@ -8,12 +8,11 @@ import (
 
 // draw border with rounded corners, on the inside of `r`.
 func drawRoundedBorder(img *draw.Image, r image.Rectangle, color *draw.Image) {
-	offset := 2
-	radius := 2 * offset
+	radius := 3
 	x0 := r.Min.X
-	x1 := r.Max.X
+	x1 := r.Max.X - 1
 	y0 := r.Min.Y
-	y1 := r.Max.Y
+	y1 := r.Max.Y - 1
 	tl := image.Pt(x0+radius, y0+radius)
 	bl := image.Pt(x0+radius, y1-radius)
 	br := image.Pt(x1-radius, y1-radius)
@@ -22,8 +21,8 @@ func drawRoundedBorder(img *draw.Image, r image.Rectangle, color *draw.Image) {
 	img.Arc(bl, radius, radius, 0, color, image.ZP, 180, 90)
 	img.Arc(br, radius, radius, 0, color, image.ZP, 270, 90)
 	img.Arc(tr, radius, radius, 0, color, image.ZP, 0, 90)
-	img.Line(image.Pt(x0, y0+offset), image.Pt(x0, y1-offset), 0, 0, 0, color, image.ZP)
-	img.Line(image.Pt(x0+offset, y1), image.Pt(x1-offset, y1), 0, 0, 0, color, image.ZP)
-	img.Line(image.Pt(x1, y1-offset), image.Pt(x1, y0+offset), 0, 0, 0, color, image.ZP)
-	img.Line(image.Pt(x1-offset, y0), image.Pt(x0+offset, y0), 0, 0, 0, color, image.ZP)
+	img.Line(image.Pt(x0, y0+radius), image.Pt(x0, y1-radius), 0, 0, 0, color, image.ZP)
+	img.Line(image.Pt(x0+radius, y1), image.Pt(x1-radius, y1), 0, 0, 0, color, image.ZP)
+	img.Line(image.Pt(x1, y1-radius), image.Pt(x1, y0+radius), 0, 0, 0, color, image.ZP)
+	img.Line(image.Pt(x1-radius, y0), image.Pt(x0+radius, y0), 0, 0, 0, color, image.ZP)
 }
