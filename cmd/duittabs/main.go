562f79162cdc644b2c9587c7a2b175f4834b6056
Mechiel Lukkien <mechiel@ueber.net>
2018-01-04 22:04:08 +0100

    add add an example of new Tabs UI

---
+++ cmd/duittabs/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+
+	"github.com/mjl-/duit"
+)
+
+func check(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s\n", msg, err)
+	}
+}
+
+func main() {
+	dui, err := duit.NewDUI("page", "800x600")
+	check(err, "new dui")
+
+	dui.Top = &duit.Tabs{
+		Buttongroup: &duit.Buttongroup{
+			Texts: []string{
+				"tab1",
+				"tab2",
+				"tab3",
+			},
+		},
+		UIs: []duit.UI{
+			&duit.Button{Text: "this is the content of tab1"},
+			&duit.Field{Text: "this is the content of tab2"},
+			&duit.Label{Text: "this is the content of tab3"},
+		},
+	}
+	dui.Render()
+
+	for {
+		select {
+		case e := <-dui.Events:
+			dui.Event(e)
+		}
+	}
+}
