c67829ed3ba3ce333eb26b94917ac1805aeb8c1e
Mechiel Lukkien <mechiel@ueber.net>
2018-01-06 20:57:43 +0100

    add linewrapping to label

---
+++ examples/label/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"image"
+	"log"
+
+	"github.com/mjl-/duit"
+)
+
+func check(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s\n", msg, err)
+	}
+}
+
+func main() {
+	dui, err := duit.NewDUI("page", "800x600")
+	check(err, "new dui")
+
+	dui.Top = &duit.Box{
+		Padding: duit.SpaceXY(6, 4),
+		Margin:  image.Pt(6, 4),
+		Valign:  duit.ValignMiddle,
+		Kids: duit.NewKids(
+			&duit.Label{
+				Text: "this is a label. we are trying to make it very long, so it will wrap. please resize the window to see this in action. also, there is a newline at the end of this sentence.\nso this should always be on the next line. end of label.",
+			},
+		),
+	}
+	dui.Render()
+
+	for {
+		select {
+		case e := <-dui.Events:
+			dui.Event(e)
+		}
+	}
+}
--- label.go
+++ label.go
@@ -9,6 +9,8 @@ import (
 type Label struct {
 	Text string
 	Font *draw.Font
+
+	lines []string
 }
 
 var _ UI = &Label{}
@@ -17,12 +19,43 @@ func (ui *Label) font(dui *DUI) *draw.Font {
 	return dui.Font(ui.Font)
 }
 
-func (ui *Label) Layout(dui *DUI, size image.Point) image.Point {
-	return ui.font(dui).StringSize(ui.Text)
+func (ui *Label) Layout(dui *DUI, sizeAvail image.Point) (sizeTaken image.Point) {
+	font := ui.font(dui)
+	ui.lines = []string{}
+	s := 0
+	x := 0
+	xmax := 0
+	for i, c := range ui.Text {
+		if c == '\n' {
+			ui.lines = append(ui.lines, ui.Text[s:i])
+			s = i + 1
+			x = 0
+			continue
+		}
+		dx := font.StringWidth(string(c))
+		x += dx
+		if i-s == 0 || x <= sizeAvail.X {
+			continue
+		}
+		xmax = maximum(xmax, x-dx)
+		ui.lines = append(ui.lines, ui.Text[s:i])
+		s = i
+		x = dx
+	}
+	if s < len(ui.Text) || s == 0 {
+		ui.lines = append(ui.lines, ui.Text[s:])
+	}
+	sizeTaken = image.Pt(xmax, len(ui.lines)*font.Height)
+	return
 }
 
 func (ui *Label) Draw(dui *DUI, img *draw.Image, orig image.Point, m draw.Mouse) {
-	img.String(orig, dui.Regular.Normal.Text, image.ZP, ui.font(dui), ui.Text)
+	p := orig
+	font := ui.font(dui)
+	for _, line := range ui.lines {
+		img.String(p, dui.Regular.Normal.Text, image.ZP, font, line)
+		p.Y += font.Height
+	}
 }
 
 func (ui *Label) Mouse(dui *DUI, origM, m draw.Mouse) Result {
