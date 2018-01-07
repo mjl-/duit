e5cc679e5136db794b0632e58c6269f3f9b7673c
Mechiel Lukkien <mechiel@ueber.net>
2018-01-07 11:03:47 +0100

    don't export Pick.UI
    left users potentially confused, whether they should set it themselves or not.

--- pick.go
+++ pick.go
@@ -8,35 +8,36 @@ import (
 
 type Pick struct {
 	Pick func(sizeAvail image.Point) UI
-	UI   UI
+
+	ui UI
 }
 
 func (ui *Pick) Layout(dui *DUI, sizeAvail image.Point) (sizeTaken image.Point) {
-	ui.UI = ui.Pick(sizeAvail)
-	return ui.UI.Layout(dui, sizeAvail)
+	ui.ui = ui.Pick(sizeAvail)
+	return ui.ui.Layout(dui, sizeAvail)
 }
 
 func (ui *Pick) Draw(dui *DUI, img *draw.Image, orig image.Point, m draw.Mouse) {
-	ui.UI.Draw(dui, img, orig, m)
+	ui.ui.Draw(dui, img, orig, m)
 }
 
 func (ui *Pick) Mouse(dui *DUI, m draw.Mouse, origM draw.Mouse) (r Result) {
-	return ui.UI.Mouse(dui, m, origM)
+	return ui.ui.Mouse(dui, m, origM)
 }
 
 func (ui *Pick) Key(dui *DUI, k rune, m draw.Mouse, orig image.Point) (r Result) {
-	return ui.UI.Key(dui, k, m, orig)
+	return ui.ui.Key(dui, k, m, orig)
 }
 
 func (ui *Pick) FirstFocus(dui *DUI) (warp *image.Point) {
-	return ui.UI.FirstFocus(dui)
+	return ui.ui.FirstFocus(dui)
 }
 
 func (ui *Pick) Focus(dui *DUI, o UI) (warp *image.Point) {
-	return ui.UI.Focus(dui, o)
+	return ui.ui.Focus(dui, o)
 }
 
 func (ui *Pick) Print(indent int, r image.Rectangle) {
 	PrintUI("Pick", indent, r)
-	ui.UI.Print(indent+1, r)
+	ui.ui.Print(indent+1, r)
 }
